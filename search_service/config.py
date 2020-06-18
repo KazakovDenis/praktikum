86334c3f38ee55ef13ffc3f440b132201fa7a388
Denis Kazakov <kazakovdeyu@yandex.ru>
2020-06-18 23:00:45 +0700

    Added logger

--- search_service/config.py
+++ search_service/config.py
@@ -1,10 +1,16 @@
 import os
 
 
+# common
 ROOT = os.path.abspath(os.curdir)
+
+# logger
 LOG_LEVEL = 30
+LOG_FILE = os.path.join(ROOT, '..', 'log', 'search_srv.log')
 LOG_FORMAT = "[%(asctime)s] @%(name)s  %(levelname)s in %(module)s: %(message)s"
+os.makedirs(LOG_FILE, exist_ok=True)
 
 
+# app
 class Config:
     DEBUG = False
