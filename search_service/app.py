346c5dabfb20e7f368f255b2b588d03a50b0093e
Denis Kazakov <kazakovdeyu@yandex.ru>
2020-06-18 23:54:14 +0700

    client blueprint

--- search_service/app.py
+++ search_service/app.py
@@ -2,8 +2,11 @@ from flask import Flask
 from flask_script import Manager
 
 from .config import *
+from .client.blueprint import client
 
 
-app = Flask(__name__)
+app = Flask('Search service')
 app.config.from_object(Config)
+app.register_blueprint(client, url_prefix='/client/')
+
 manager = Manager(app)
---
+++ search_service/client/blueprint.py
@@ -0,0 +1,15 @@
+import json
+from flask import Blueprint, Response, request
+from ..helpers import logger
+
+
+client = Blueprint('client', __name__)
+
+
+@client.route('info')
+def info():
+    logger.info(f'REQUEST FROM: {request.remote_addr}')
+    user_agent = request.user_agent.to_header()
+    content = json.dumps({'user_agent': user_agent})
+    response = Response(content, content_type='application/json')
+    return response
