5402005ddcdb61b48e811974b89448eb4b821557
Denis Kazakov <kazakovdeyu@yandex.ru>
2020-06-20 16:34:32 +0700

    blueprints circular import

--- search_service/app.py
+++ search_service/app.py
@@ -3,14 +3,16 @@ from flask_script import Manager
 from common import get_logger
 
 from .config import *
-from .api.blueprint import api
-from .client.blueprint import client
 
 
 app = Flask(__package__)
 app.config.from_object(Config)
-app.register_blueprint(api, url_prefix='/api/v1/')
-app.register_blueprint(client, url_prefix='/client/')
-
 manager = Manager(app)
 logger = get_logger(__package__, LOG_FILE)
+
+# blueprints
+from .api.blueprint import api
+from .client.blueprint import client
+
+app.register_blueprint(api, url_prefix='/api/v1/')
+app.register_blueprint(client, url_prefix='/client/')
