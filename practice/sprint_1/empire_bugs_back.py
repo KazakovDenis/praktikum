182e6172e8c02fac91b97dc78c438a35ca58a71b
Denis Kazakov <kazakovdeyu@yandex.ru>
2020-06-20 18:05:12 +0700

    sp1 unique_tags

--- practice/sprint_1/empire_bugs_back.py
+++ practice/sprint_1/empire_bugs_back.py
@@ -1,27 +1,34 @@
 from json import loads
-from typing import List
 
 
 request_payload = """\
 { 
 "title": "Звездные войны 1: Империя приносит баги",
 "description": "Эпичная сага по поиску багов в старом легаси проекте Империи",
-"tags": [2, "семейное кино", "космос", 1.0, "сага", "эпик", "добро против зла", true, "челмедведосвин", "debug", "ipdb", "PyCharm", "боевик", "боевик", "эникей", "дарт багус", 5, 6,4, "блокбастер", "кино 2020", 7, 3, 9, 12, "каникулы в космосе", "коварство"],
+"tags": [
+    2, "семейное кино", "космос", 1.0, "сага", "эпик", "добро против зла", true, "челмедведосвин", "debug", "ipdb", 
+    "PyCharm", "боевик", "боевик", "эникей", "дарт багус", 5, 6,4, "блокбастер", "кино 2020", 7, 3, 9, 12, 
+    "каникулы в космосе", "коварство"
+    ],
 "version": 17
 }
 """
 
 
-def unique_tags(payload: dict) -> List[str]:
-    result = []
+def unique_tags(payload: dict) -> list:
+    """Extracts unique tags from payload"""
     tags = payload.get('tags', [])
+    unique = list(set(tags))
 
-    for tag in tags:
-        if tag not in result or type(result[result.index(tag)]) != type(tag):
-            result.append(tag)
+    int_inherited = filter(
+        lambda tag: type(tag) != type(unique[unique.index(tag)]),
+        tags
+    )
 
-    return result
+    unique.extend(int_inherited)
+    return unique
 
 
 converted = loads(request_payload)
 result = unique_tags(converted)
+print(result)
