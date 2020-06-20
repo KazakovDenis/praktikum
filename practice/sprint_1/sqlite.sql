faa6b12c4f1ca1e4e985fe32b3dcc76c7ffaec1b
Denis Kazakov <kazakovdeyu@yandex.ru>
2020-06-20 23:31:30 +0700

    sqlite writers query

--- practice/sprint_1/sqlite.sql
+++ practice/sprint_1/sqlite.sql
@@ -1,3 +1,4 @@
+-- Actors who Jørgen Lerdam has deal with
 SELECT a.name
     FROM movies m
     JOIN movie_actors ma
@@ -6,3 +7,14 @@ SELECT a.name
         ON ma.actor_id = a.id
     WHERE m.director LIKE '%Lerdam%';
 
+
+-- Writers who have took part in most films
+SELECT writer, MAX(amount) as movies_amount
+    FROM (
+        SELECT w.name as writer, count(m.id) as amount
+            FROM writers w
+            JOIN movies m
+            ON w.id = m.writer
+            WHERE w.name != 'N/A'
+            GROUP BY w.name
+    )
