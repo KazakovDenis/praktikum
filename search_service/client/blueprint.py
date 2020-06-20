8f7fd79a114ad225c3b733d7814e0e9669fd5d9d
Denis Kazakov <kazakovdeyu@yandex.ru>
2020-06-20 16:30:19 +0700

    __package__ > appname

--- search_service/client/blueprint.py
+++ search_service/client/blueprint.py
@@ -1,4 +1,4 @@
-import json
+from json import dumps as json_dumps
 from flask import Blueprint, Response, request
 from ..app import logger
 
@@ -10,6 +10,6 @@ client = Blueprint('client', __name__)
 def info():
     logger.info(f'REQUEST FROM: {request.remote_addr}')
     user_agent = request.user_agent.to_header()
-    content = json.dumps({'user_agent': user_agent})
+    content = json_dumps({'user_agent': user_agent})
     response = Response(content, content_type='application/json')
     return response
