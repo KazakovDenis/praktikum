2c8db7a91e4f97512582388bb6d83f0ca172e2a3
Denis Kazakov <kazakovdeyu@yandex.ru>
2020-06-25 16:13:46 +0700

    api get_movie

--- search_service/api/blueprint.py
+++ search_service/api/blueprint.py
@@ -1,9 +1,23 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from ..app import es, logger
 
 
 api = Blueprint('api', __name__)
 
 
-@api.route('video')
-def video():
-    return '<h1>Video endpoint</h1>'
+@api.route('movies')
+def search():
+    q = request.args.get('q')
+    return '<h1>Movies endpoint</h1>'
+
+
+@api.route('movies/<movie_id>')
+def get_movie(movie_id):
+
+    query = """{
+        "query": {
+            "term":  { "id": "%s" }
+        }
+    }""" % movie_id
+    result = es.search(query, index='movies')
+    return result['hits']['hits'][0]['_source']
--- search_service/app.py
+++ search_service/app.py
@@ -1,7 +1,8 @@
+from elasticsearch import Elasticsearch
 from flask import Flask
 from flask_script import Manager
-from common import get_logger
 
+from common import get_logger, ES_HOSTS
 from .config import *
 
 
@@ -9,6 +10,8 @@ app = Flask(__package__)
 app.config.from_object(Config)
 manager = Manager(app)
 logger = get_logger(__package__, LOG_FILE)
+
+es = Elasticsearch(ES_HOSTS)
 
 # blueprints
 from .api.blueprint import api
