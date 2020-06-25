48a99816ce0f2a6abddcfc89df41b73569c82cb7
Denis Kazakov <kazakovdeyu@yandex.ru>
2020-06-25 15:33:48 +0700

    small fixes

--- common/config.py
+++ common/config.py
@@ -3,5 +3,5 @@ import os.path as op
 
 ROOT = op.abspath(op.curdir)
 ES_HOSTS = ['http://127.0.0.1:9200']
-DB_ADDRESS = op.abspath(op.join('practice', 'sprint_1', 'db.sqlite'))
-JSON_DIR = op.abspath(op.join('practice', 'sprint_1'))
+DB_ADDRESS = op.join(ROOT, 'practice', 'sprint_1', 'db.sqlite')
+JSON_DIR = op.join(ROOT, 'practice', 'sprint_1')
