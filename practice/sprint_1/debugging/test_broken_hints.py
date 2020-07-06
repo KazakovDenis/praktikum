7d49d8b52beb2a4b375b84bd6961fc13d1ed5e14
Denis Kazakov <kazakovdeyu@yandex.ru>
2020-07-06 09:48:07 +0700

    pytest tests add pop size

--- practice/sprint_1/debugging/test_broken_hints.py
+++ practice/sprint_1/debugging/test_broken_hints.py
@@ -1,5 +1,10 @@
+from math import ceil, sqrt
 from pytest import fixture, main, mark, param, raises
 from practice.sprint_1.debugging.broken_hints import Matrix
+
+
+TYPES = 1, 1.0, 'string', {}, set(), bytes(), bytearray()
+PARAMS = (param(item, id=type(item).__name__) for item in TYPES)
 
 
 @fixture
@@ -7,33 +12,33 @@ def matrix():
     return Matrix()
 
 
-@mark.parametrize(
-    'item',
-    [
-        param(1, id='Int item'),
-        param(1.0, id='Float item'),
-        param('Str', id='Str item'),
-        param({}, id='Dict item'),
-        param(set(), id='Set item'),
-        param(bytes(), id='Bytes item'),
-        param(bytearray(), id='Bytearray item'),
-    ]
-)
-def test_add_item_positive(item, matrix):
+@mark.parametrize('item', PARAMS)
+def test_add_item_positive(matrix, item):
     matrix.add_item(item)
     assert str(matrix) == f'{item} None\nNone None'
 
 
-@mark.parametrize(
-    'item',
-    [
-        param(None, id='None item'),
-        param(id='No item'),
-    ]
-)
-def test_add_item_negative(item, matrix):
+def test_add_item_negative(matrix):
     with raises(ValueError):
-        matrix.add_item()
+        matrix.add_item(None)
+
+
+@mark.parametrize('item', PARAMS)
+def test_pop_positive(matrix, item):
+    matrix.add_item(item)
+    assert matrix.pop() == item
+
+
+def test_pop_negative(matrix):
+    with raises(IndexError):
+        matrix.pop()
+
+
+@mark.parametrize('size', (i ** 4 for i in range(10)))
+def test_size(matrix, size):
+    for i in range(size):
+        matrix.add_item(1)
+    assert matrix.size == ceil(sqrt(size) + 1)
 
 
 if __name__ == '__main__':
