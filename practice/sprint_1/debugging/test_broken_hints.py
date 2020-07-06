96b1c28a3de06a0d645c204cb948cd77fb24e542
Denis Kazakov <kazakovdeyu@yandex.ru>
2020-07-06 10:18:38 +0700

    pytest tests str

--- practice/sprint_1/debugging/test_broken_hints.py
+++ practice/sprint_1/debugging/test_broken_hints.py
@@ -12,6 +12,14 @@ def matrix():
     return Matrix()
 
 
+@fixture
+def matrix1000():
+    matrix = Matrix()
+    for i in range(1000):
+        matrix.add_item(1)
+    return matrix
+
+
 @mark.parametrize('item', PARAMS)
 def test_add_item_positive(matrix, item):
     matrix.add_item(item)
@@ -39,6 +47,20 @@ def test_size(matrix, size):
     for i in range(size):
         matrix.add_item(1)
     assert matrix.size == ceil(sqrt(size) + 1)
+
+
+def test_str(matrix1000):
+    array = [1 for i in range(1000)]
+    array += [None for i in range(89)]
+
+    matrix = []
+    for i in range(33):
+        start, end = i * 33, (i + 1) * 33
+        row = ' '.join(map(lambda x: str(x), array[start:end]))
+        matrix.append(row)
+
+    expected = '\n'.join(matrix)
+    assert str(matrix1000) == expected
 
 
 if __name__ == '__main__':
