e3bf57b4b73fce1fa82759a66a9dde066ca14ff7
Denis Kazakov <kazakovdeyu@yandex.ru>
2020-07-09 16:32:25 +0700

    test_client

--- search_service/tests/test_smoke.py
+++ search_service/tests/test_smoke.py
@@ -1,41 +1,26 @@
-from json import loads as json_loads
-from multiprocessing import Process
-from time import sleep
-
-from pytest import mark
-from requests import get as http_get
-
+from pytest import fixture, mark
 from search_service.manage import *
 
 
-HOST, PORT = '127.0.0.1', 7999
-BASE_URL = f'http://{HOST}:{PORT}'
-server = Process(target=lambda: app.run(host=HOST, port=PORT))
-
-
-def setup_module():
-    server.start()
-    sleep(5)        # while the server is not running
-
-
-def teardown_module():
-    server.kill()
-    server.join()
+@fixture
+def client():
+    return app.test_client()
 
 
 @mark.parametrize('url', [
-    BASE_URL,
-    BASE_URL + '/api/v1/movies/',
-    BASE_URL + '/api/v1/movies/tt0112270',
+    '/',
+    '/api/v1/movies/',
+    '/api/v1/movies/tt0112270',
 ])
-def test_service_running(url):
+def test_service_running(client, url):
     """Checks that url returns HTTP 200"""
-    response = http_get(url)
-    assert response.status_code == 200
+    with client:
+        response = client.get(url)
+        assert response.status_code == 200
 
 
-def test_client_info():
+def test_client_info(client):
     """Check that ClientInfo endpoint works correctly"""
-    response = http_get(BASE_URL + '/client/info')
-    payload = json_loads(response.content)
-    assert 'python-requests' in payload.get('user_agent', '')
+    with client:
+        response = client.get('/client/info', headers={'User-Agent': 'testagent/1.0'})
+        assert response.json == {'user_agent': 'testagent/1.0'}
